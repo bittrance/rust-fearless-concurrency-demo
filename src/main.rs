1effb355b34568282d59d6f76567dbe46485ea3d
Anders Qvist <anders.qvist@xenit.se>
2022-04-08 15:15:49 +0200

    Single-threaded array version, refactored.
    Simple program to sum up key,value tuples. For clarity, the
    actual counting is separated into a separate function.
    
    This is nice, but of course we would want to parallelize this.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,26 @@
+use std::env::args;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
+fn update(sums: &mut [usize], input: File) {
+    let reader = BufReader::new(input);
+    for row in reader.lines().map(Result::unwrap) {
+        if let Some((key_str, val_str)) = row.split_once(',') {
+            let key: usize = key_str.parse().unwrap();
+            let val: usize = val_str.parse().unwrap();
+            sums[key] += val;
+        } else {
+            panic!("Bad line: {}", row);
+        }
+    }
+}
+
 fn main() {
     let mut sums = [0usize; 10];
-    let files_names = ["file-1", "file-2"];
+    let files_names = args().into_iter().skip(1);
     for name in files_names {
         let input = File::open(name).unwrap();
-        let reader = BufReader::new(input);
-        for row in reader.lines().map(Result::unwrap) {
-            if let Some((key_str, val_str)) = row.split_once(',') {
-                let key: usize = key_str.parse().unwrap();
-                let val: usize = val_str.parse().unwrap();
-                sums[key] += val;
-            } else {
-                panic!("Bad line: {}", row);
-            }
-        }
+        update(&mut sums, input);
     }
     println!("{:?}", sums);
 }
